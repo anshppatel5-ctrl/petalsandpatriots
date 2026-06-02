5b779688e103fbb1fe7814b46febb490739609e1
anshppatel5-ctrl <anshppatel5@gmail.com>
2026-06-01 21:10:42 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-refresh' // or '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/petals-and-patriots-site/',
+})
